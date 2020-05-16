68e14640092372da9df590318c9a2199d66b3ff9
Yatin T <yatint5@gmail.com>
2020-05-17 00:24:28 +0530

    envs check backup working correctly, deletion and backup to be checked

--- app.py
+++ app.py
@@ -1,102 +1,194 @@
+import os
+import pickle
+from utils import *
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import tkinter.font as font
+from tkinter import Checkbutton,Canvas
+from tkinter.messagebox import askyesno,askquestion,showinfo
 
-root = tk.Tk()
-root.title("System App Manager")
-root.wm_iconbitmap("logo.ico")
+if not os.path.exists('repos'):
+	os.mkdir('repos')
 
-global bgColor
-bgColor = "#142952"
-apps = []
+if os.path.exists('saved_session.pickle'):
+	pickle_in = open('saved_session.pickle',"rb")
+	saved_session = pickle.load(pickle_in)
+else:
+	saved_session = {}
+	update_session_file(saved_session)
 
-if os.path.isfile('yourApps.txt'):
-    with open('yourApps.txt', 'r') as f:
-        tempApps = f.read()
-        tempApps = tempApps.split(',')
-        apps = [x for x in tempApps if x.strip()]
+root = Tk()
 
+width_of_window = 650
+height_of_window = 480
 
-#========== Functions for buttons ==========#
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-# Function for loading app list in UI
+x = (screen_width/2)-(width_of_window/2)
+y = (screen_height/2)-(height_of_window/2)
 
-def loadUI():
-    for app in apps:
-        listBox.insert("end", app)
+root.geometry("%dx%d+%d+%d"%(width_of_window,height_of_window,x,y))
 
-# Function for selecting applications
+# <----------------functions------------------>
+def init_repo():
+	url_value = url.get()
+	repo_name = init_local_repo(url_value)
+	saved_session['repo_name'] = repo_name
+	update_session_file(saved_session)
 
+# <----------------Notebook------------------>
+note = ttk.Notebook(root)
 
-def selectApp():
+home = ttk.Frame(note)
+tab1 = ttk.Frame(note)
+tab2 = ttk.Frame(note)
 
-    filename = filedialog.askopenfilename(initialdir='C:', title='Select Application',
-                                          filetypes=(("executables", "*.exe"), ("all files", "*.*")))
-
-    listBox.delete(0, "end")
-
-    apps.append(filename)
-    loadUI()
-
-
-# Funciton for running application
-
-
-def runApp():
-    for app in apps:
-        os.startfile(app)
-
-
-# Function for deleting an application
-
-def deleteApp():
-    app = listBox.get("active")
-    if app in apps:
-        apps.remove(app)
-        listBox.delete(0, "end")
-
-    loadUI()
-
-# Function for clearing application list
+note.add(home,text="Add Github url")
+note.add(tab1, text = "Your envs")
+note.add(tab2, text = "Envs on Github", state="disabled")
+note.pack(expand = 1, fill = "both")
 
 
-def clearList():
-    apps.clear()
-    listBox.delete(0, "end")
+# <----------------Home------------------>
+canvas_home = Canvas(home,bg='#398FFF')
+canvas_home.pack(expand=True,fill=BOTH)
 
-    loadUI()
+url = Entry(canvas_home,width=40)
+url['font']=font.Font(size=15)
 
+enter_url_button = Button(canvas_home, text="Enter Url",command=init_repo)
+enter_url_button['font']=font.Font(size=15)
 
-#========== Main App Canvas ==========#
-canvas = tk.Canvas(root, height=500, width=800, bg=bgColor)
-canvas.pack()
-
-
-#========== Center Frame ==========#
-listBox = tk.Listbox(root, bg="white")
-listBox.place(relwidth=0.8, relheight=0.7, relx=0.1, rely=0.12)
+url.place(relx=0.5, rely=0.4, anchor=CENTER)
+enter_url_button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
-#========== Buttons ==========#
+# <----------------tab1------------------>
 
-# Button for clearing apps list
-clearListButton = tk.Button(root, text="Clear List", padx=10,pady=5, fg="white", bg=bgColor, command=clearList)
-clearListButton.pack(side=tk.RIGHT)
+canvas_tab1 = Canvas(tab1,bg='#398FFF')
+canvas_tab1.pack(expand=True,fill=BOTH)
 
-# Button for deleting apps
-deleteButton = tk.Button(root, text="Delete App", padx=10,pady=5, fg="white", bg=bgColor, command=deleteApp)
-deleteButton.pack(side=tk.RIGHT)
+envs = get_envs()
+env_list = Listbox(canvas_tab1,width = 50,bg='#85144B',fg='white')
+env_list.pack(side=LEFT,fill=Y)
+env_list.config(border=2,relief=FLAT,font=("Courier", 9))
+for env in envs:
+	env_list.insert(END,"      "+env)
+scroll = Scrollbar(canvas_tab1,orient=VERTICAL,command=env_list.yview)
+scroll.pack(side=LEFT,fill=Y)
 
-# Button for selecting apps
-selectButton = tk.Button(root, text="Select Apps", padx=10,pady=5, fg="white", bg=bgColor, command=selectApp)
-selectButton.pack(side=tk.RIGHT)
+env_list['yscrollcommand'] = scroll.set
 
-# Button for running apps
-runButton = tk.Button(root, text="Run Apps", padx=10,pady=5, fg="white", bg=bgColor, command=runApp)
-runButton.pack(side=tk.RIGHT)
+def show_info_backup(env_name,version):
+	showinfo(title="Backedup successfully!", message=env_name+" backedup successfully to version"+version)
+
+def get_env_name_from_selection():
+	print('env_list.curselection()',env_list.curselection())
+	item = list(map(int, env_list.curselection()))
+	print('item',item)
+	env_name = env_list.get(item)
+	# env_name = env_list.get(ANCHOR)
+
+	env_name = env_name.split(' ')[-1]
+	print('env_name :: ',env_name)
+	return env_name
+
+def check_backup():
+	print('saved_session["repo_name"]',saved_session['repo_name'])
+	env_name = get_env_name_from_selection()
+	path_to_env = os.path.join('repos',saved_session['repo_name'],env_name)
+	if not os.path.exists(path_to_env):
+		answer = askyesno('Not backedup',env_name+" is not backedup. Do u wish to backup?")
+		if answer:
+			version = save_env(env_name,saved_session['repo_name'],os.path.join('repos',saved_session['repo_name']))
+			show_info_backup(env_name,version)
+	else:
+		backed_up = check_prev_backup(path_to_env,env_name)
+		if backed_up:
+			showinfo(title="Backedup!", message=env_name+"is backedup")
+		else:
+			answer = askyesno('Backedup outdated',env_name+" has changed since the last backup. Do u wish to backup again?")
+			if answer:
+				version = save_env(env_name,saved_session['repo_name'],os.path.join('repos',saved_session['repo_name']))
+				show_info_backup(env_name,version)
+
+def backup_env():
+	env_name = get_env_name_from_selection()
+	version = save_env(env_name,saved_session['repo_name'],os.path.join('repos',saved_session['repo_name']))
+	show_info_backup(env_name,version)
+	
+
+def delete_env():
+	env_name = get_env_name_from_selection()
+	backed_up = check_prev_backup(path_to_env,env_name)
+	if backed_up:
+		remove_env(env_name)
+		showinfo(title="Removed!", message=env_name+"is removed from ur local machine")
+		env_list.delete(env_list.curselection())
+	else:
+		showwarning()
+		answer = askquestion ("Env not backed up!!",env_name+"has not been backed up or has been modified afterwards, are you sure you want to remove it?",icon = 'warning')
+		if answer=="yes":
+			remove_env(env_name)
+			showinfo(title="Removed!", message=env_name+"is removed from ur local machine")
+			env_list.delete(env_list.curselection())
 
 
-loadUI()
+check = Button(canvas_tab1, text="Check if backedup",command=check_backup,relief=FLAT,padx=20,pady=10,fg='blue',bd=2)
+check.pack(side=TOP,pady=90)
+check['font']=font.Font(size=15)
 
-root.mainloop()
 
-with open('yourApps.txt', 'w') as f:
-    for app in apps:
-        f.write(app + ',')
+backup = Button(canvas_tab1, text="Backup env", command=backup_env,relief=FLAT,padx=20,pady=10,fg='blue',bd=2)
+backup.pack(side=TOP,pady=10)
+backup['font']=font.Font(size=15)
+
+
+delete = Button(canvas_tab1, text="Delete env", command=delete_env,relief=FLAT,padx=20,pady=10,fg='blue',bd=2)
+delete.pack(side=TOP,pady=10)
+delete['font']=font.Font(size=15)
+
+
+# <----------------tab2------------------>
+# if 'repo_name' in saved_session.keys():
+# 	if not saved_session['repo_name'] == None:
+# 		note.tab(0, state="normal")
+
+# 		canvas = Canvas(tab2,bg='#398FFF')
+# 		canvas.pack(expand=True,fill=BOTH)
+
+# 		envs = envs_in_repo(saved_session['repo_name'])
+# 		env_list = Listbox(canvas,width = 50,bg='#85144B',fg='white')
+# 		env_list.pack(side=LEFT,fill=Y)
+# 		env_list.config(border=2,relief=FLAT,font=("Courier", 9))
+# 		for env in envs:
+# 			env_list.insert(END,"      "+env)
+# 		scroll = Scrollbar(canvas,orient=VERTICAL,command=env_list.yview)
+# 		scroll.pack(side=LEFT,fill=Y)
+
+# 		env_list['yscrollcommand'] = scroll.set
+
+
+
+
+if __name__ == "__main__":
+	root.mainloop()
+
+
+
+
+
+
+# canvas = Canvas(tab1 ,bg='green')#, width=200, height=400
+# scroll = Scrollbar(tab1, command=canvas.yview)
+# canvas.config(yscrollcommand=scroll.set, scrollregion=(0,0,100,1000))
+# canvas.pack(side=LEFT, fill=BOTH, expand=True)
+# scroll.pack(side=RIGHT, fill=Y)
+
+# frame = Frame(canvas, bg='red')
+# canvas.create_window(90, 450, window=frame)
+
+# frame2 = Frame(canvas,bg="red")
+# canvas.create_window(2000, 550, window=frame2)
--- utils.py
+++ utils.py
@@ -1,36 +1,119 @@
 import os
+import pickle
 import subprocess
 
 def init_local_repo(url):
-    subprocess.run(f'git clone {url}',shell=True)
+    print('cloning :',url)
+    subprocess.run(f'git clone {url} repos/{get_repo_name_from_url(url)}',shell=True)
     repo_name = url.split('/')[-1].split('.')[0]
-    assert os.path.exists(repo_name)
+    assert os.path.exists(os.path.join('repos',repo_name))
     return repo_name
+
+def get_repo_name_from_url(url):
+    return url.split('/')[-1].split('.')[0]
 
 def pull_remote():
     subprocess.run(f'git pull',shell=True)
 
 def get_envs():
     output = subprocess.check_output('conda env list', universal_newlines=True,shell=True)
-    env = []
+    envs = []
     lines = output.split('\n')
     lines = lines[2:]
     for line in lines:
-        env.append(line.split(' ')[0])
+        env = line.split(' ')[0]
+        if not env == '':
+            envs.append(env)
     return envs
 
-def save_env(env,path):
-    file_path = os.path.join(path,f'{env}.txt')
-    subprocess.run(f"conda env export --from-history --name {env} > {file_path}",shell=True)
-    assert os.path.exists('{}.txt'.format(env))
+def save_env(env,repo_name,path):
+    path_to_env = os.path.join(path,env)
+    if not os.path.exists(path_to_env):
+        version = '0'
+        os.makedirs(path_to_env)
+    else:
+        version = get_latest_version_number(path_to_env)
+
+    path_to_env_version = os.path.join(path_to_env,version)
+    os.mkdir(path_to_env_version)
+
+    file_path = os.path.join(path_to_env_version,env+'.yml')
+    subprocess.run(f"conda env export --name {env} > {file_path}",shell=True)
+    assert os.path.exists(file_path)
+
+    file_path_from_history = os.path.join(path_to_env_version,env+'_from_history.yml')
+    subprocess.run(f"conda env export --from-history --name {env} > {file_path_from_history}",shell=True)
+    assert os.path.exists(file_path_from_history)
+
+    if version == '0':
+        update_github(env,repo_name,path_to_env,"add new env")
+    else:
+        update_github(env,repo_name,path_to_env,"add version",version=version)
+    return version
 
 def remove_env(env):
     subprocess.run(f"conda remove --name {env} --all",shell=True)
     envs = get_envs()
     assert env not in envs
 
-def push_env_to_github(env,path):
-    file_path = os.path.join(path,f'{env}.txt')
-    subprocess.run(f"git add {file_path}",shell=True)
-    subprocess.run(f'git commit -m "add {env}"',shell=True)
-    subprocess.run(f'git push',shell=True)
+def envs_in_repo(repo_name):
+    return os.listdir(os.path.join('repos',repo_name))
+
+def list_of_repos():
+    return os.listdir('repos')
+
+def update_session_file(saved_session):
+    pickle_out = open("saved_session.pickle",'wb')
+    pickle.dump(saved_session, pickle_out)
+    pickle_out.close()
+
+def get_latest_version_number(path_to_env):
+    list_of_versions = os.listdir(path_to_env)
+    print(list_of_versions)
+    if len(list_of_versions) == 0:
+        return 0
+    else:
+        return str(max(list(map(int,list_of_versions))) + 1)
+
+def save_env_temp(env):
+    subprocess.run(f"conda env export --name {env} > temp.yml",shell=True)
+    assert os.path.exists('temp.yml')
+
+    file_path_from_history = 'temp_from_history.yml'
+    subprocess.run(f"conda env export --from-history --name {env} > {file_path_from_history}",shell=True)
+    assert os.path.exists(file_path_from_history)
+
+def check_prev_backup(path_to_env,env):
+    if not os.path.exists(path_to_env):
+        return False
+    latest_version = str(int(get_latest_version_number(path_to_env)) - 1)
+    path_to_yml_file = os.path.join(path_to_env,latest_version,env+'.yml')
+    save_env_temp(env)
+
+    with open(path_to_yml_file) as f1:
+        with open('temp.yml') as f2:
+            if f1.read() == f2.read():
+                return True
+            else:
+                return False
+
+def update_github(env,repo_name,path_to_env,action,version=None):
+    workingdir = os.getcwd()
+    os.chdir(os.path.join('repos',repo_name))
+    if action=="add version":
+        path_to_env_version = os.path.join(env,version)
+        subprocess.run(f"git add {path_to_env_version}",shell=True)
+        subprocess.run(f'git commit -m "add {env} version {version}"',shell=True)
+        subprocess.run(f'git push',shell=True)
+
+    elif action=="remove env backup":
+        subprocess.run(f"git add {env}",shell=True)
+        subprocess.run(f'git commit -m "removed {env}"',shell=True)
+        subprocess.run(f'git push',shell=True)
+
+    elif action=="add new env":
+        subprocess.run(f"git add {env}",shell=True)
+        subprocess.run(f'git commit -m "add {env} version 0"',shell=True)
+        subprocess.run(f'git push',shell=True)
+
+    os.chdir(workingdir)
